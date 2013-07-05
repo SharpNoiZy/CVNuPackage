39ffffbbf9dd19311a0465baae25ae64135f211f
chneu <cn_web_accounts@yahoo.de>
2013-07-05 16:42:24 +0200

    Initial commit

---
+++ CVNuPackage/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CVNuPackage
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var version                 = "";
+			var version_check_file      = args[0];
+			var nuspec_file             = "";
+			var additional_parameters   = "";
+			FileVersionInfo versionInfo = null;
+
+
+			try
+			{
+				//! Check if given dll or exe file, which should be checked for version, exists
+				if (!File.Exists(version_check_file))
+					throw new Exception("File for version specificication not found!\r\n[" + version_check_file + "]");
+				
+
+				//! Get version and bring it into the correct format
+				versionInfo = FileVersionInfo.GetVersionInfo(version_check_file);
+				version = versionInfo.FileMajorPart.ToString() + "." + versionInfo.FileMinorPart.ToString() + "." + versionInfo.FileBuildPart.ToString();
+
+
+				//! Check if .nuspec file is provided via command parameter or not and handle it
+				if (args.Length == 1 || (args.Length > 1 && args[1] == ""))
+				{
+					var exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+					nuspec_file = Directory.GetFiles(exePath, "*.nuspec").FirstOrDefault();
+					if (nuspec_file == null) throw new Exception("No .nuspec file found!\r\n");
+				}
+				else if (args.Length > 1 && args[1] != "")
+				{
+					nuspec_file = args[1];
+
+					if (!args[1].Contains("\\"))
+						nuspec_file = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), args[1]);
+
+					if (!nuspec_file.EndsWith(".nuspec"))
+						nuspec_file += ".nuspec";
+
+					if (!File.Exists(nuspec_file))
+						throw new Exception(".nuspec file does not exist!\r\n[" + nuspec_file + "]");
+				}
+
+
+				//! Check if additional NuGet parameters are supplied
+				if(args.Length > 2)
+				{
+					if (!args[2].StartsWith(" "))
+						args[2] = " " + args[2];
+
+					additional_parameters = args[2];
+				}
+
+
+				//! Execute nuget command line tool with given parameters
+				ProcessStartInfo startInfo = new ProcessStartInfo();
+				startInfo.CreateNoWindow   = false;
+				startInfo.UseShellExecute  = false;
+				startInfo.FileName         = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "nuget.exe");
+				startInfo.WindowStyle      = ProcessWindowStyle.Hidden;
+				startInfo.Arguments        = "pack \"" + nuspec_file + "\" -Version " + version + additional_parameters;
+
+
+				using (Process exeProcess = Process.Start(startInfo))
+				{
+					exeProcess.WaitForExit();
+				}
+			}
+			catch (Exception ex)
+			{
+				File.AppendAllText(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "log.txt"), "\r\n\r\n" + DateTime.Now.ToString() + ":\r\n" + ex.Message + "");
+			}
+		}
+	}
+}
---
+++ CVNuPackage/readme.txt
@@ -0,0 +1,29 @@
+﻿This litte tool is very easy to use.
+
+Requirements:
+1. NuGet.exe console application have to be in the same directory like this application.
+	- Is provided by this application, but can maybe outdated some time. 
+
+
+How to:
+1. Open the project which you want to create as a NuGet package
+2. Open the project properties
+3. Open the "Buildevents"
+4. Under the Postbuild section enter a string looking like the example below, that's it
+
+
+Example: 
+1. "C:\Path\CVNuPackage.exe" "$(TargetPath)"
+2. "C:\Path\CVNuPackage.exe" "$(TargetPath)" "" "-OutputDirectory \"C:\My NuGet Path\""
+
+
+Explanation:
+First parameter: Path to this little application
+Second parameter: Path to the .dll or .exe file from which you want to take the version number
+Optional (Required if a 4th parameter is given) third parameter:
+	1. Leave complete, like the first example above. 
+		- ".nuspec" file is expected in the same directory like this tool
+	2. Specify a .nuspec file path with filename (".nuspec" extension is not required)
+		- If your ".nuspec" file is in a different directory that this tool
+	3. If a 4th parameter is given you have two possibilities
+		1)
